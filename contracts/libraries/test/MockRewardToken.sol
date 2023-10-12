8ac4e25721ae241337a42c9b69df1940ce74dfef
PhillGray <phil_gray92@live.dk>
2023-10-12 12:08:53 +0200

    remove 3 arg

--- contracts/libraries/test/MockRewardToken.sol
+++ contracts/libraries/test/MockRewardToken.sol
@@ -3,14 +3,13 @@ pragma solidity 0.8.19;
 
 import "../token/ERC20.sol";
 
-
 // This contract represents a mock stable coin for testing purposes
 contract MockRewardToken is ERC20 {
-    constructor() ERC20("MockRewardToken", "mREWARD", 18) {
-        _mint(msg.sender, 1000000 * 10**decimals()); // Mint some initial tokens for testing
+    constructor() ERC20("MockRewardToken", "mREWARD") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint some initial tokens for testing
     }
 
-    function mint(address to, uint256 amount) external  {
+    function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 }
