e4071c25cdae004e26d62382be81577d26346a2a
PhillGray <phil_gray92@live.dk>
2023-10-17 22:49:27 +0200

    all but 2 tests missing to pass

--- test/FerroRewards_Tests.js
+++ test/FerroRewards_Tests.js
@@ -6,33 +6,50 @@ describe("FerroRewards", function () {
   let deployer;
   let recipient1;
   let recipient2;
-
-  before(async function () {
-    
-  });
+  let rewardToken;
+  let mockIronNFT;
+  let mockNickelNFT;
+  let mockCobaltNFT;
 
   beforeEach(async function () {
     [deployer, recipient1, recipient2] = await ethers.getSigners();
 
-    // Deploy the NFT contracts 
-    const ironNFTContract = "0x1234567890123456789012345678901234567890";
-    const nickelNFTContract = "0x9876543210987654321098765432109876543210";
-    const cobaltNFTContract = "0x5678901234567890123456789012345678901234";
-    
+    // Deploy mock Token contract for testing
     const RewardToken = await ethers.getContractFactory("MockRewardToken");
     rewardToken = await RewardToken.deploy();
-    
 
+    // Deploy mock NFT contracts for testing
+    const MockIronNFT = await ethers.getContractFactory("MockIronNFT", deployer);
+    mockIronNFT = await MockIronNFT.deploy(deployer);
+
+    const MockNickelNFT = await ethers.getContractFactory("MockNickelNFT", deployer);
+    mockNickelNFT = await MockNickelNFT.deploy(deployer);
+
+    const MockCobaltNFT = await ethers.getContractFactory("MockNickelNFT", deployer);
+    mockCobaltNFT = await MockCobaltNFT.deploy(deployer);
+
+    const mockIronAddress = await mockIronNFT.getAddress();
+    const mockNickelAddress = await mockNickelNFT.getAddress();
+    const mockCobaltAddress = await mockCobaltNFT.getAddress();
+
+    // Mint NFTs to simulate ownership
+    await mockIronNFT.safeMint(recipient1.getAddress(), 1);
+    await mockNickelNFT.safeMint(recipient1.getAddress(), 1);
+    await mockCobaltNFT.safeMint(recipient1.getAddress(), 1);
+
+    // Mint Iron NFT for recipient2
+    await mockIronNFT.safeMint(recipient2.getAddress(), 2);
+
+    // Deploy the main contract with the mock NFT contract addresses
     const FerroRewards = await ethers.getContractFactory("FerroRewards");
     contract = await FerroRewards.deploy(
-      ironNFTContract,
-      nickelNFTContract,
-      cobaltNFTContract
+      mockIronAddress,
+      mockNickelAddress,
+      mockCobaltAddress
     );
-      
     return {contract, deployer, recipient1, recipient2};
-  });
 
+  });
   it("should deploy the contract", async function () {
     await expect(contract.address).to.not.equal(0);
   });
@@ -99,4 +116,35 @@ it("Should revert when non-owner tries to unpause the contract", async function 
     expect(nickelBalance).to.equal(nickelAllocation);
     expect(cobaltBalance).to.equal(cobaltAllocation);
   });
+
+  it("should perform airdrop of tokens correctly", async function () {
+    const tokenAddress = await rewardToken.getAddress();
+    const amount = 10000;
+
+    // Deposit tokens and simulate NFT ownership
+    await rewardToken.connect(deployer).approve(contract.getAddress(), amount);
+    await contract.connect(deployer).depositTokens(amount, tokenAddress);
+    
+    // Simulate NFT ownership for recipients
+    await contract.connect(deployer).addIronNFT(recipient1.address, 1);
+    await contract.connect(deployer).addNickelNFT(recipient1.address, 1);
+    await contract.connect(deployer).addCobaltNFT(recipient1.address, 1);
+
+    await contract.connect(deployer).addIronNFT(recipient2.address, 1);
+
+    // Set the airdrop duration and interval
+    await contract.connect(deployer).updateAirdropCount(6, 1);
+
+    // Perform the airdrop
+    await contract.connect(deployer).airdropTokens();
+
+    // Check recipient1's token balance after airdrop
+    const recipient1Balance = await rewardToken.balanceOf(recipient1.address);
+    expect(recipient1Balance).to.equal(3000); // 3 hours x 1000 tokens
+
+    // Check recipient2's token balance after airdrop
+    const recipient2Balance = await rewardToken.balanceOf(recipient2.address);
+    expect(recipient2Balance).to.equal(1000); // 1 hour x 1000 tokens
+  });
+
 });
